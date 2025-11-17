46ef109277787a046251ac5dfb668a3f8f89ea0b
itssadiq <sadiqalishaikh009@gmail.com>
2025-11-17 12:45:48 +0500

    created store and basic reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import { Button } from "./components/ui/button";
 import RegistrationForm from "./RegistrationForm";
+import Todo from "./Todo";
 function App() {
   return (
     <>
-      <RegistrationForm />
+      <Todo />
     </>
   );
 }
